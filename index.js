255f15b751e0079b0ef109183f425a53d94a322f
vdiez <diez@outlook.com>
2018-01-03 10:53:58 +0100

    avoid declaring wamp on every retry

--- index.js
+++ index.js
@@ -14,32 +14,30 @@ WAMP.prototype = {
     run(method, params, sync = false, timeout = 60000) {
         if (!method || !params) throw ("Missing mandatory fields");
         let self = this;
-        return new Promise(function(resolve, reject) {
+        return new Promise((resolve, reject) => {
             self.queue = Promise.resolve(self.queue)
                 .then(() => {
                     if (self.session) return;
-                    return new Promise(function (resolve2, reject2) {
-                        let connect = function () {
-                            let wamp = new autobahn.Connection({url: self.router, realm: self.realm, max_retries: 0});
-                            wamp.onopen = function (session) {
-                                winston.info("WAMP session established with " + self.router);
-                                self.session = session;
-                                resolve2();
-                            };
-                            wamp.onclose = function (reason, details) {
-                                if (!self.session) {
-                                    winston.warn("WAMP session could not be established with " + self.router + ". Error: " + reason);
-                                    setTimeout(connect, 5000);
-                                }
-                                else winston.warn("WAMP session lost with " + self.router + ". Error: " + reason);
-                                self.session = undefined;
-                            };
-                            wamp.open();
+                    return new Promise((resolve2, reject2) => {
+                        let wamp = new autobahn.Connection({url: self.router, realm: self.realm, max_retries: 0});
+                        let connect = () => wamp.open();
+                        wamp.onopen = session => {
+                            winston.info("WAMP session established with " + self.router);
+                            self.session = session;
+                            resolve2();
+                        };
+                        wamp.onclose = (reason, details) => {
+                            if (!self.session) {
+                                winston.warn("WAMP session could not be established with " + self.router + ". Error: " + reason);
+                                setTimeout(connect, 5000);
+                            }
+                            else winston.warn("WAMP session lost with " + self.router + ". Error: " + reason);
+                            self.session = undefined;
                         };
                         connect();
                     });
                 })
-                .then(function () {
+                .then(() => {
                     if (self.session.hasOwnProperty(method)) return reject("Non-recognized WAMP procedure: " + method);
                     let result = self.session[method](...params);
                     resolve(result);
@@ -54,7 +52,7 @@ WAMP.prototype = {
 };
 
 let instances = {};
-module.exports = function(router, realm, method, params, sync = false) {
+module.exports = (router, realm, method, params, sync = false) => {
     if (router && realm) {
         let key = router + ":" + realm;
         if (!instances.hasOwnProperty(key)) instances[key] = WAMP(router, realm);
