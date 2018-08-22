e7d729b40d7f7a8bbc30ca5ce78fd3270f638afa
vdiez <diez@outlook.com>
2018-08-22 12:45:31 +0200

    refactor connection to avoid multiple reconnection at the same time, the one from automatic reconnection and the one from current call (connection promise was already resolved if connection was lost) bugfix: router should be the last parameter to be copied

--- index.js
+++ index.js
@@ -6,60 +6,58 @@ function WAMP(router, realm) {
     wamp_instance.router = router;
     wamp_instance.realm = realm;
     wamp_instance.session = false;
-    wamp_instance.connecting = false;
-    wamp_instance.closed = true;
+    wamp_instance.connection = false;
+    wamp_instance.closed = true; //used as sometimes autobahn calls onclose twice
     wamp_instance.procedures = {};
     wamp_instance.subscriptions = {};
     wamp_instance.onopen = () => {};
     wamp_instance.onclose = () => {};
     wamp_instance.queue = undefined;
+    wamp_instance.wamp = new autobahn.Connection({url: wamp_instance.router, realm: wamp_instance.realm, max_retries: 0});
+    wamp_instance.wamp.onopen = (session, details) => {
+        winston.info("WAMP session established with " + wamp_instance.router);
+        for (let procedure in wamp_instance.procedures) {
+            if (wamp_instance.procedures.hasOwnProperty(procedure)) wamp_instance.run("register", wamp_instance.procedures[procedure]);
+        }
+        for (let subscription in wamp_instance.subscriptions) {
+            if (wamp_instance.subscriptions.hasOwnProperty(subscription)) wamp_instance.run("subscribe", wamp_instance.subscriptions[subscription]);
+        }
+        wamp_instance.session = session;
+        wamp_instance.resolve_connection(wamp_instance);
+        wamp_instance.onopen(wamp_instance);
+    };
+    wamp_instance.wamp.onclose = (reason, details) => {
+        if (wamp_instance.closed) return;
+        wamp_instance.closed = true;
+        if (wamp_instance.session) wamp_instance.onclose(wamp_instance);
+        winston.warn("WAMP session could not be established with " + wamp_instance.router + ". Error: " + reason);
+        wamp_instance.connection = false;
+        if (!wamp_instance.session || Object.keys(wamp_instance.procedures).length || Object.keys(wamp_instance.subscriptions).length) setTimeout(() => {wamp_instance.connect();}, 5000);
+        wamp_instance.session = undefined;
+    };
+    wamp_instance.resolve_connection = undefined;
     return wamp_instance;
 }
 
 WAMP.prototype = {
+    connect() {
+        if (this.session && this.session.isOpen) return this;
+        if (!this.connection) this.connection = new Promise(resolve => {
+            if (this.connection) return resolve(this.connection);
+            winston.verbose("WAMP session starting with " + this.router);
+            this.closed = false;
+            this.resolve_connection = resolve;
+            this.wamp.open();
+        });
+        return this.connection;
+    },
     run(method, params, sync = false, timeout = 60000) {
-        let key, failed = false;
+        let key, rejected = false;
         return new Promise((resolve, reject) => {
             this.queue = Promise.resolve(this.queue)
-                .then(() => {
-                    if (this.session && this.session.isOpen) return;
-                    return new Promise((resolve2, reject2) => {
-                        let wamp = new autobahn.Connection({url: this.router, realm: this.realm, max_retries: 0});
-                        let connect = () => {
-                            if (this.connecting) return;
-                            winston.verbose("WAMP session starting with " + this.router);
-                            this.connecting = true;
-                            this.closed = false;
-                            wamp.open();
-                        };
-                        wamp.onopen = (session, details) => {
-                            winston.info("WAMP session established with " + this.router);
-                            for (let procedure in this.procedures) {
-                                if (this.procedures.hasOwnProperty(procedure)) this.run("register", this.procedures[procedure]);
-                            }
-
-                            for (let subscription in this.subscriptions) {
-                                if (this.subscriptions.hasOwnProperty(subscription)) this.run("subscribe", this.subscriptions[subscription]);
-                            }
-
-                            this.onopen();
-                            this.session = session;
-                            resolve2(this);
-                        };
-                        wamp.onclose = (reason, details) => {
-                            if (this.closed) return;
-                            if (this.session) this.onclose();
-                            this.closed = true;
-                            this.connecting = false;
-                            winston.warn("WAMP session could not be established with " + this.router + ". Error: " + reason);
-                            if (!this.session || Object.keys(this.procedures).length || Object.keys(this.subscriptions).length) setTimeout(connect, 5000);
-                            this.session = undefined;
-                        };
-                        connect();
-                    });
-                })
+                .then(() => this.connect())
                 .then(session => {
-                    if (!method || !params) return session;
+                    if (!method || !params) return resolve(session);
                     if (method === "unregister" || method === "unsubscribe") key = params.shift();
                     new Promise((resolve2, reject2) => {
                         let exec = () => {
@@ -69,7 +67,7 @@ WAMP.prototype = {
                                 result.then(result => resolve2(result))
                                     .catch(err => {
                                         winston.error("WAMP error: ", err);
-                                        if (err && err.error === "wamp.error.no_such_procedure" && !failed) setTimeout(() => exec(), 5000);
+                                        if (err && err.error === "wamp.error.no_such_procedure" && !rejected) setTimeout(() => exec(), 5000);
                                         else reject2(err);
                                     });
                             }
@@ -97,7 +95,7 @@ WAMP.prototype = {
             if (!sync) resolve();
             else {
                 if (typeof timeout === "number") setTimeout(() => {
-                    failed = true;
+                    rejected = true;
                     reject({timeout: true});
                 }, timeout);
             }
@@ -115,8 +113,9 @@ module.exports = (router, realm, method, params, sync = false, timeout) => {
         params = router.params;
         method = router.method;
         realm = router.realm;
-        router = router.router;
+        timeout = router.timeout;
         connect = router.connect;
+        router = router.router;
     }
     if (router && realm) {
         let key = router + ":" + realm;
@@ -124,7 +123,7 @@ module.exports = (router, realm, method, params, sync = false, timeout) => {
         if (typeof onopen === "function") instances[key].onopen = onopen;
         if (typeof onclose === "function") instances[key].onclose = onclose;
         if (method && params) return instances[key].run(method, params, sync, timeout);
-        else if (connect) return instances[key].run(null, null, true, timeout);
+        else if (connect) return instances[key].run(null, null, sync, timeout);
         else return instances[key];
     }
     throw ("Missing mandatory fields");
